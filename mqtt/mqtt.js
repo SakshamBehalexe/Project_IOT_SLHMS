b52ff337fdcc3507d3c3b4b4d839b06129137ab3
SakshamBehalexe <106488190+SakshamBehalexe@users.noreply.github.com>
2023-05-01 11:30:56 +0530

    commit

--- mqtt/mqtt.js
+++ mqtt/mqtt.js
@@ -19,6 +19,10 @@ app.use((req, res, next) => {
   next();
 });
 
+const port = 5008;
+
+const mqttLink = "http://localhost:5008"
+
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
@@ -28,9 +32,11 @@ app.use(function(req, res, next) {
 app.use(bodyParser.json()); 
 app.use(bodyParser.urlencoded({ extended: true }));
 
+const client = mqtt.connect('mqtt://broker.hivemq.com:1883');
+
 const statusSchema = new mongoose.Schema({
   status: {
-    type: Boolean,
+    type: String,
     required: true
   },
 
@@ -42,62 +48,28 @@ const statusSchema = new mongoose.Schema({
 
 const Status = mongoose.model('slhms_status', statusSchema);
 
-//schema for temperature
-const Temperature = mongoose.model('Temperature', new mongoose.Schema({
-
-  tempValue: {
-     type: Number
-   },
-  HumValue: {
-     type: Number
-   },
-   recordedAt: {
-     type: Date,
-     default: Date.now
-
-   }}, { collection : 'Temperature_2' }));
-
-
-
-   const client = mqtt.connect("mqtt://broker.hivemq.com:1883");
-
 app.get('/testing', async (req, res) => {
   res.send("Testing");
 });
 
 client.on('connect', () => { 
   console.log('MQTT connected');
+  client.subscribe('lecturehall/status');
 });
 
-const port = 5008;
-
-client.on('message', async (topic, message)=> {
-  if (topic === "temp_slhm") {
-    console.log(`Received message on topic "${topic}": ${message.toString()}`);
-    // Parse the message payload
-    const data = JSON.parse(message);
-
-    // Save temperature data to MongoDB using Mongoose
-    const newTemperature = new Temperature({
-      tempValue: data.temperature,
-      HumValue: data.humidity,
-      recordedAt: Date.now()
-    });
-
-    try {
-      await newTemperature.save();
-      console.log(`Temperature data saved to MongoDB`);
-    } catch (err) {
-      console.error(`Error saving temperature data to MongoDB: ${err}`);
-      if (err.errors) {
-        console.error(`Validation errors: ${err.errors}`);
-      }
-    }
-  }
-}); 
+app.post('/lecturehall/status', async (req, res) => {
+  const { status } = req.body;
+  
+  const newStatus = new Status({ status });
+  await newStatus.save();
+  
+  client.publish('lecturehall/status', JSON.stringify({ status }));
+  
+  res.send("Status updated successfully");
+});
 
 app.listen(port, () => { 
-  console.log(`listening on port ${port}`);
+  console.log(`listening on port ${port} link to the mqtt server ${mqttLink}`);
 });
 
 
